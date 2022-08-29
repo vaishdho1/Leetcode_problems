94b8748f0cb00300261eb5ed8fae41c891c94e70
Vaishnavi Panchavati <38342947+vaishdho1@users.noreply.github.com>
2022-08-29 15:30:37 +0530

    Create 113_Path_Sum_II.cpp

---
+++ 113_Path_Sum_II.cpp
@@ -0,0 +1,51 @@
+/*
+DFS is run from the root till the leaf.
+Add the value to a list everytime we enter into a node.
+Everytime a leaf node is reached,we check if the sum of the nodes
+in the path is same as the targetsum.
+If the sum is the same, insert the list into the ans
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    
+    void final_path(TreeNode* root, int targetSum,vector<int>path, vector<vector<int> >&ans)
+    {
+       
+        path.push_back(root->val);
+        if(root->left==NULL && root->right==NULL && targetSum-root->val==0)
+        {    ans.push_back(path);
+             return;
+        }
+        else if(root->left==NULL && root->right==NULL)
+            return;
+        if(root->left!=NULL)
+            final_path(root->left, targetSum-root->val,path,ans);
+        if(root->right!=NULL)
+             final_path(root->right,targetSum-root->val,path,ans);
+        return;
+            
+        
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int> >ans;
+        if(root==NULL)
+            return ans;
+        vector<int>path;
+        final_path(root,targetSum,path,ans);
+        return ans;
+    }
+};
